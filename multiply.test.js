5e349cd838f1971b7d1153a6f7bd24207c6a2da6
Nikolay Komarov <kny.komarov@gmail.com>
2024-04-06 00:20:32 +0300

    добавит примеры вариантов проверок

--- multiply.test.js
+++ multiply.test.js
@@ -6,6 +6,20 @@ describe('multiply', () => {
     const expectedResult = 6;
     
     expect(expectedResult).toBe(actualResult);
+
+    // варианты проверок
+    expect({a: 1}).toEqual({a: 1});
+    expect([1, 2, 3]).toHaveLength(3);
+    expect([1, 2, 3]).toContain(3);
+    expect([1, 2, 3]).not.toContain(10);
+
+    expect(undefined).toBeUndefined();
+    expect(null).toBeNull();
+    expect(0).toBeFalsy();
+    expect('').toBeFalsy();
+
+    expect(actualResult).toBeDefined();
+    expect(actualResult).toBeTruthy();
   });
 
   it('should multiply negative numbers', () => {
