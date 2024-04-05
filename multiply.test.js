3b73de9e185b220961327c44ee56cad862ec8a20
Nikolay Komarov <kny.komarov@gmail.com>
2024-04-06 00:01:05 +0300

    добавит стркутуру тестов

--- multiply.test.js
+++ multiply.test.js
@@ -1,7 +1,24 @@
 import { multiply } from './multiply';
 
-test('multiply', () => {
-  const result = multiply(3, 2);
+describe('multiply', () => {
+  it('should multiply positive numbers', () => {
+    const actualResult = multiply(3, 2);
+    const expectedResult = 6;
+    
+    expect(expectedResult).toBe(actualResult);
+  });
 
-  expect(result).toBe(6);
+  it('should multiply negative numbers', () => {
+    const actualResult = multiply(-3, -2);
+    const expectedResult = 6;
+    
+    expect(expectedResult).toBe(actualResult);
+  });
+
+  it('should multiply positive and negative numbers', () => {
+    const actualResult = multiply(3, -2);
+    const expectedResult = -6;
+    
+    expect(expectedResult).toBe(actualResult);
+  });
 });
